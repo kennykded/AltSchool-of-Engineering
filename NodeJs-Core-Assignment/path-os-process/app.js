0e454fe49343f71aa054dbcd4542f659ff67737f
kennykded <82447341+kennykded@users.noreply.github.com>
2023-10-16 16:27:50 +0100

    Edit comments

--- NodeJs-Core-Assignment/path-os-process/app.js
+++ NodeJs-Core-Assignment/path-os-process/app.js
@@ -6,30 +6,30 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const sampleFilePath = join(__dirname, 'my-files')
-const sampleFile = join(sampleFilePath, 'sample.kded')
+const sampleFile = join(sampleFilePath, 'sample.txt')
 
-// // for getting current working directory using resolve function from path module
-// const cwd = resolve()
-// console.log({cwd})
+// print out the current working directory using resolve function from path module
+const cwd = resolve()
+console.log({cwd})
 
 // print out the seperator of a given file path
 const seperator = sep
 console.log({seperator})
 
-// print out the extension of a given file path
+// print out the extension name of a file path
 const pathExtension = extname(sampleFile)
 console.log({pathExtension})
 
 console.log({sampleFilePath})
 
-// print out the current procees id
+// print out the current procees id of the current working process
 const processId = pid
 console.log('Current Process ID:', processId)
 
-// prints userinformation of the os
+// print out the user information of the os
 const userInformation = userInfo()
 console.log({userInformation})
 
-// print out the os platform
+// print out the platform of an operating system
 const osPlatform = platform()
 console.log({osPlatform})
